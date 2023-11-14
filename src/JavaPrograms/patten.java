0ce708c03c96dbba8760aedd8ecfcb0fc0719b63
DeveloperSummit <anujs@TheDevSummit>
2023-11-14 15:22:09 +0530

    patten

---
+++ src/JavaPrograms/patten.java
@@ -0,0 +1,12 @@
+package JavaPrograms;
+
+public class patten {
+
+    public static void main(String[] args) {
+
+        for (int i=0;i<10;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
